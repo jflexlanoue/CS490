02063681bfe9a60162f90683ef9abd07174419a3
George Hahn <george@genericmaker.com>
2016-07-12 02:17:37 -0400

    Add session debugging script

--- Backend/dependencies/session_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'database.php';
-
-$result["username"] = $_SESSION["username"];
-$result["authenticated"] = $_SESSION["authenticated"];
-$result["permission"] = $_SESSION["permission"];
---
+++ Backend/session_info.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'database.php';
+
+if(session_status() == PHP_SESSION_ACTIVE)
+{
+    $response["username"] = $_SESSION["username"];
+    $response["authenticated"] = $_SESSION["authenticated"];
+    $response["permission"] = $_SESSION["permission"];
+}
+else
+    Error("No active session");
