6c6481b5d28233e746efe76eb1e46a8ee5f9c6fc
George Hahn <george@genericmaker.com>
2016-07-12 03:27:26 -0400

    Scratchpad for exam API

---
+++ Backend/exam.php
@@ -0,0 +1,16 @@
+<?php
+require 'common.php';
+
+must_be_instructor();
+
+switch($_SERVER['REQUEST_METHOD'])
+{
+    case "GET":
+        break;
+    case "POST":
+        break;
+    case "PATCH":
+        break;
+    case "DELETE":
+        break;
+}
