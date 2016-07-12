e7f031eb9a86e4c452254786944de82f614a666d
George Hahn <george@genericmaker.com>
2016-07-12 03:44:46 -0400

    Add basic exam persistence

--- Backend/exam.php
+++ Backend/exam.php
@@ -6,8 +6,27 @@ must_be_instructor();
 switch($_SERVER['REQUEST_METHOD'])
 {
     case "GET":
+        if(isset($_REQUEST["id"]))
+        {
+            $response["result"] = R::load('exam', $_REQUEST["id"]);
+
+            if($response["result"]["id"] == 0)
+            {
+                unset($response["result"]);
+                Error("No exams found");
+            }
+        } else {
+            $response["result"] = R::findAll('exam');
+        }
         break;
     case "POST":
+        $exam = R::dispense('exam');
+        $exam->title = $_REQUEST["title"];
+        $exam->released = $_REQUEST["released"];
+
+        if(isset($_REQUEST["questionIDs"]))
+            $exam->questionIDs = $_REQUEST["questionIDs"]; // TODO
+        R::store($exam);
         break;
     case "PATCH":
         break;
