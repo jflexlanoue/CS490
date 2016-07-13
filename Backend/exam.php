dcc47c3c33e49ad7db878da5d8fde8a1c68b5bf7
George Hahn <george@genericmaker.com>
2016-07-13 03:27:45 -0400

    Finish question list handling

--- Backend/exam.php
+++ Backend/exam.php
@@ -7,8 +7,16 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case "GET":
         if (isset($_REQUEST["id"])) {
             $response["result"] = load_or_error('exam', $_REQUEST["id"]);
+
+            // Expand questions
+            $response["result"]->sharedQuestionList;
         } else {
             $response["result"] = R::findAll('exam');
+
+            // Expand questions
+            foreach($response["result"] as $exam) {
+                $exam->sharedQuestionList;
+            }
         }
         break;
 
@@ -18,9 +26,15 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $exam = R::dispense('exam');
         $exam->title = $_REQUEST["title"];
         $exam->released = filter_var($_REQUEST["released"], FILTER_VALIDATE_BOOLEAN);
+        $exam->sharedQuestionList = array();
 
-        if (isset($_REQUEST["questionIDs"]))
-            $exam->questionIDs = $_REQUEST["questionIDs"]; // TODO: parse IDs, verify questions exist
+        if (isset($_REQUEST["questionIDs"])) {
+            $questions = explode(",", $_REQUEST["questionIDs"]);
+            foreach($questions as $question) {
+                array_push($exam->sharedQuestionList, load_or_error('question', (int)$question));
+            }
+        }
+
         $response["result"] = R::store($exam);
         break;
 
@@ -33,8 +47,14 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $exam->title = $_REQUEST["title"];
         if (isset($_REQUEST["released"]))
             $exam->released = filter_var($_REQUEST["released"], FILTER_VALIDATE_BOOLEAN);
-        if (isset($_REQUEST["questionIDs"]))
-            $exam->questionIDs = $_REQUEST["questionIDs"]; // TODO: parse IDs, verify questions exist
+
+        if (isset($_REQUEST["questionIDs"])) {
+            $exam->sharedQuestionList = array();
+            $questions = explode(",", $_REQUEST["questionIDs"]);
+            foreach($questions as $question) {
+                array_push($exam->sharedQuestionList, load_or_error('question', (int)$question));
+            }
+        }
         R::store($exam);
         break;
 
