bd9b21c4025ca242de4c29fcf319749d749c0d9e
jflexlanoue <jflexlanoue@gmail.com>
2016-07-16 23:49:12 -0400

    Create util.class.php

---
+++ MiddleEnd/util.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class util { 
+    
+
+   const UTIL_BASEURL = "https://web.njit.edu/~glh4/";
+
+    static function httpPost($url, $data)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
+    static function IsLoggedIn(){
+        $url = self::UTIL_BASEURL . "session_info.php";
+        
+        $res = self::httpPost($url, array());
+        $dec_res = json_decode($res);
+        
+        var_dump($dec_res);
+
+    }
+    
+    static function LogInUser($Username, $Password){
+
+        $url = self::UTIL_BASEURL . "backend_login.php";
+
+        $Data =  array("username" => $Username, "password" => $Password );
+        $res = self::httpPost($url, $Data);
+        $dec_res = json_decode($res);
+        return $dec_res;
+
+    }
+
+}
