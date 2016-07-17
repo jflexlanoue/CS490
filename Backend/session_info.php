5373ce03e767ef6e34157c1c2e0e9b1e5082415e
George Hahn <george@genericmaker.com>
2016-07-17 00:19:19 -0400

    Improve invalid session handling

--- Backend/session_info.php
+++ Backend/session_info.php
@@ -2,6 +2,9 @@
 require 'common.php';
 
 if (session_status() == PHP_SESSION_ACTIVE) {
+    if(!isset($_SESSION["username"])) {
+        Error("Session active, but session variable is empty");
+    }
     $response["username"] = $_SESSION["username"];
     $response["authenticated"] = $_SESSION["authenticated"];
     $response["permission"] = $_SESSION["permission"];
