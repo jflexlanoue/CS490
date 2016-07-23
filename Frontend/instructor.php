52edcae13e47f7d42d93b1bfa812534a4b3cd868
garykc <gkc@protonmail.com>
2016-07-23 13:01:48 -0400

    Create instructor.php

---
+++ Frontend/instructor.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Instructor</title>
+    </head>
+    <body>
+        <a href="edit_user.php">Account settings</a><br>
+        <center>
+            <h1>Instructor</h1>
+            <a href="question_creation.php">Question Creation</a><br>
+            <a href="exam_creation.php">New Exam</a><br>
+            <h1>Published Exams</h1>
+            <br>
+        </center>
+    </body>
+</html>
