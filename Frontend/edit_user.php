a62cf3e952d51743d2aa4444fcc20decb754d24e
garykc <gkc@protonmail.com>
2016-07-23 13:00:07 -0400

    Create edit_user.php

---
+++ Frontend/edit_user.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Student</title>
+    </head>
+    <body>
+        <a href="edit_user.php">Account settings</a><br>
+        <center>
+            <h1>Student</h1>
+        </center>
+    </body>
+</html>
