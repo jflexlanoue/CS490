4b432e7042cd8d2935ada4617369622411483603
garykc <gkc@protonmail.com>
2016-07-23 13:00:38 -0400

    Create create_user.php

---
+++ Frontend/create_user.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Registration</title>
+    </head>
+    <body>
+        <a href="index.php">Return to login page</a><br>
+        <center>
+            <h2>Registration</h2>
+            <input id="username" type="text" name="username" placeholder="username" autofocus><br><br>
+            <input id= "password" type="password" name="password" placeholder="password"><br><br>
+            <input name="loginButton" type="submit" value="Create new account" onclick=""><br><br>
+            <form action="">
+                <input type="radio" name="accType" value="Student" checked> Student<br>
+                <input type="radio" name="accType" value="Instructor"> Instructor<br>
+            </form>
+        </center>
+    </body>
+</html>
