41bef4a4d01db83317edc88f123eb99a411091b8
garykc <gkc@protonmail.com>
2016-07-23 13:01:35 -0400

    Create exam_creation.php

---
+++ Frontend/exam_creation.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Exam Creation</title>
+    </head>
+    <body>
+        <a href="instructor.php">Return to main page</a><br>
+    </body>
+</html>
+<?php
+    echo 'this is new exam';
+?>
