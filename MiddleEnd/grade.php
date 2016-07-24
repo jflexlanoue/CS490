752886b5eda6aeb33fc66bfb646ecf1fa4fa5f95
jflexlanoue <jflexlanoue@gmail.com>
2016-07-24 01:31:12 -0400

    Update grade.php

--- MiddleEnd/grade.php
+++ MiddleEnd/grade.php
@@ -6,94 +6,125 @@ error_reporting(E_ALL);
 session_start();
 
 include("util.class.php");
+include("grading.class.php");
 
 
-if(isset($_POST["Question"])){
-    $Question = nl2br($_POST['Question']);
-    $Answer = nl2br($_POST['Answer']); 
-    $StudentAnswer = nl2br($_POST['StudentAnswer']);
+$login = json_decode(util::ForwardPostRequest("backend_login.php", ["username" => "admin" , "password" => "admin"]),1);
+
+$GETPARAMS = array();
     
-} else{
+if(isset($_GET['examid'])){
+    $ExamId = $_GET['examid'];
+    $GETPARAMS = ["examID" => $ExamId ,
+                  "expand" => 1];
+}
+
+$Results = json_decode(util::ForwardGETRequest("result.php",$GETPARAMS ),1);
+
+if(!$Results['success']){
+    echo "No Results";
+    return;
+}
+
+$Results = $Results['result'];
+
+foreach($Results as  $v){
     
-    $Question = "public class Vehicle
-
-{
-
-  public void Drive(int miles)
-
-  {
-
-    // throw not implemented exception
-
-  }
-
-}";
+    $Result_ID = $v['id'];
     
-    $Answer ="public class Car extends Vehicle
-
-{
-
-  public void Drive(int miles)
-
-  {
-
-    System.out.println(miles + \" miles\");
-
-  }
-
-}";
+    $sAnswer = $v['student_answer'];
+    $sSolution = $v['question']['answer'];
+    $sQuestion = $v['question']['question'];
     
-    $StudentAnswer = "public class Car extends Vehicle
-
-{
-
-  public void Drive(int miles)
-
-  {
-
-    System.out.println(miles + \" miles\");
-
-  }
-
-}";
+    echo "Solution: " . $sSolution;
     
+        echo "<br />";
+    echo "<br />";
     
+        echo "Student Answer: " . $sAnswer;
     
+            echo "<br />";
+    echo "<br />";
+    
+    $Grade = new grading($sSolution, $sAnswer);
+    $Grade->DoGrading();
+    $FeedBack = $Grade->GetFeedBack();
+
+    echo "Score: " . $Grade->ScorePerc;
+
+        echo "<br />";
+    echo "<br />";
+
+    
+    foreach ($FeedBack as $f) {
+        echo $f;
+            echo "<br />";
+    echo "<br />";
+    }
+        echo "<br />";
+    echo "<br />";
+    
+
+    //PATCH  id => $Result_ID , score => $Score , feedback => $feedback
+}
+
+
+/*
+$exam = json_decode(util::ForwardGETRequest("exam.php",$GETPARAMS ),1);
+
+$QuestionPairs = $exam['result']['sharedQuestion'];
+
+
+foreach($QuestionPairs as $k => $v){
+    
+    $QuestionId = $v['id'];
+    $QuestionQ = $v['question'];
+    $QuestionA = $v['answer'];
+    
+
+    echo  $QuestionId . " " . $QuestionQ . " " . $QuestionA ;
+    echo "<br />";
+    echo "<br />";
     
 }
 
+$GETPARAMS = ["examID" => $ExamId];
+$result = json_decode(util::ForwardGETRequest("result.php",$GETPARAMS ),1);
+
+$ExamAnswers = $result;
+
+
+foreach($ExamAnswers as $k => $v){
+    
+    //echo  $k . "   " . $v ;
+    echo "<br />";
+    echo "<br />";
+    
+}
+
+
+
+
+
 ?>
 
-<form action="" method="POST">
-    
-    <textarea type="text" name = "Question"></textarea>
-    <textarea type="text" name = "Answer"></textarea>
-    <textarea type="text" name = "StudentAnswer"></textarea>
-    <input type="submit" value="submit">
-    
-</form>
 
-<?php 
 
-    
+
+
+<?php
+//Set Exam Released
+
+
+
+/*
+$Grade = new grading($Answer, $StudentAnswer);
+$Grade->DoGrading();
+$FeedBack = $Grade->GetFeedBack();
+
+echo $Grade->ScorePerc;*/
+
+
+
+//['studentID', 'examID', 'questionID', 'score', 'studentAnswer', 'feedback'];
 ?>
-
-
-
-<table style="width:100%">
-    <tr>
-        <td>Question</td>
-        <td>Answer</td>
-        <td>Student Answer</td>
-        <td>Grade</td>
-        <td>FeedBack</td>
-    </tr>
-    
-    <tr>
-        <td><pre><?php echo  $Question?></pre></td>
-        <td><pre><?php echo  $Answer?></pre></td>
-        <td><pre><?php echo  $StudentAnswer?></pre></td>
-
-    </tr>
-
-</table>
