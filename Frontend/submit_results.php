88d240280e9ec2d95846050f3d0c6c562386ba39
George Hahn <george@genericmaker.com>
2016-07-25 00:16:58 -0400

    Result adding is complete

--- Frontend/submit_results.php
+++ Frontend/submit_results.php
@@ -5,4 +5,28 @@ include("htmlutil.php");
 
 util::VerifyRole('student');
 
-print_r($_POST);
+$exam_id = $_POST["examid"];
+$user_id = json_decode(util::ForwardGetRequest("user.php")["result"])->id;
+
+foreach ($_POST as $item => $answer) {
+    if(substr($item, 0, 6) === "answer") {
+        $question_id = (int)substr($item, 6);
+
+        $result = Array();
+        $result["studentID"] = $user_id;
+        $result["examID"] = $exam_id;
+        $result["questionID"] = $question_id;
+        $result["score"] = 0;
+        $result["studentAnswer"] = $answer;
+        $result["feedback"] = "";
+
+        $results = util::ForwardPostRequest("result.php", $result);
+        if($results["success"] != true) {
+            $resulterror = $results["error"];
+        }
+    }
+}
+if(isset($resulterror))
+    die($resulterror);
+
+util::Redirect("student.php");
