83a9f1b7d7a0a7d1d46b5ee8cde949fdc92b617a
George Hahn <george@genericmaker.com>
2016-07-25 01:50:00 -0400

    Add UI half of exam editing

--- Frontend/exam_edit.php
+++ Frontend/exam_edit.php
@@ -3,45 +3,79 @@ include("Garyutil.class.php");
 include("htmlutil.php");
 util::VerifyRole('instructor');
 hdr("Exam Creation");
+
+$exam_id = $_GET["id"];
+$creation = !isset($exam_id);
+
+// Do edit
+if($_SERVER['REQUEST_METHOD'] === "POST") {
+    print_r($_POST);
+
+    //util::Redirect('instructor.php');
+}
+
+if(!$creation) {
+    $exam = util::ForwardGETRequest("exam.php", array("id" => $exam_id));
+    if(!$exam['success']) {
+        die($exam["error"]);
+    }
+    $exam = $exam["result"];
+    $exam_question_ids = array();
+    foreach ($exam["sharedQuestion"] as $item => $value) {
+        array_push($exam_question_ids, $value["id"]);
+    }
+} else {
+    $exam = array();
+    $exam["title"] = "";
+    $exam["released"] = "";
+    $exam["sharedQuestion"] = array();
+}
+
+$questionRetrieval = util::ForwardGETRequest("question.php", array());
+
+if( !$questionRetrieval['success']) {
+    die($questionRetrieval["error"]);
+}
+$questions = $questionRetrieval["result"];
 ?>
 
 <a href="instructor.php">Return to main page</a>
 
 <center>
-    <h2>Create Exams</h2>
+    <h2><?php echo ($creation ? "Create Exams" : "Edit exam") ?></h2>
 
-    <?php
+    <form method="post">
+        <?php if($creation) echo '<input type="hidden" name="examid" value="' . $exam_id . '">' ?>
+        <div>
+            <label>Name</label>
+            <input name="name" value="<?php echo $exam["title"] ?>">
+        </div>
+        <div>
+            <label>Released</label>
+            <input name="released" <?php if($exam["released"] == 1) echo 'checked="checked"'?> type="checkbox">
+        </div>
 
-    $questionRetrieval = util::ForwardGETRequest("question.php", array());
+        <table>
+            <tr>
+                <td>Included</td>
+                <td>Question</td>
+                <td>Answer</td>
+            </tr>
+            <?php
+            foreach ($questions as $q) {
+                echo '<tr>';
+                echo '<td><input name="' . $q['id'] . '" type="checkbox" ' . (in_array($q['id'], $exam_question_ids) ? 'checked="checked"' : "") . '></td>';
+                echo '<td>' . $q['question'] . '</td>';
+                echo '<td>' . $q['answer'] . '</td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
 
-    if( !$questionRetrieval['success']) {
-
-        echo 'error';
-        //return;
-    }
-    ?>
-
-    <table>
-        <tr>
-            <!-- <td></td> -->
-            <td>id</td>
-            <td>Question</td>
-            <td>Answer</td>
-        </tr>
-        <?php
-        $i = 1;
-        foreach ( $questionRetrieval['result'] as $q ){       // $q is an array
-            echo '<tr>';
-            // echo '<input type="checkbox" name="check_list[]" value="PHP">';
-            echo '<td>' . $q['id'] . '</td>';
-            echo '<td>' . $q['question'] . '</td>';
-            echo '<td>' . $q['answer'] . '</td>';
-            echo '</tr>';
-            $i++;
-        }
-        ?>
-    </table>
-
+        <div>
+            <button type="submit"><?php echo ($creation ? "Create" : "Edit") ?></button>
+        </div>
+    </form>
 </center>
 
 <?php
