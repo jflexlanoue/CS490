34500ea88b394220c4d21bce0f65b0c1f1f3ec9c
George Hahn <george@genericmaker.com>
2016-07-27 18:57:43 -0400

    Move questions to template

--- Frontend/question_creation.php
+++ Frontend/question_creation.php
@@ -4,84 +4,37 @@ include("Util/htmlutil.php");
 
 util::VerifyRole("instructor");
 
-$message = "";
-
 if (isset($_POST['question'])) {
-
     $question = $_POST['question'];
     $answer = $_POST['answer'];
 
     if ($question == "") {
-        $message = 'Question cannot be left blank.';
+        $view["message"] = 'Question cannot be left blank.';
     } else if ($answer == "") {
-        $message = 'Answer cannot be left blank.';
+        $view["message"] = 'Answer cannot be left blank.';
     } else {
-
         $response = util::ForwardPostRequest("question.php", $_POST);
-
         if($response['success']){
-            $message = 'Question and answer added successfully.';
+            $view["message"] = 'Question and answer added successfully.';
         } else{
-            $message = $response['error'];
+            $view["message"] = $response['error'];
         }
     }
 }
 
-$template = "instructor_template";
-hdr("Question Creation");
-?>
+if (isset($_GET['edit'])) {
+    $editing = true;
+    $model["id"] = $_GET['edit'];
+    $model =$response = util::ForwardGetRequest("question.php", $model);
+}
 
-    <center>
-        <h1>Question Creation</h1>
-        <br>
-        <div id="message">
-            <?php
-            if ($message != "") {
-                echo $message;
-            }
-            ?>
-        </div>
-        <br>
-        <form action = "question_creation.php" method="POST">
-            <label for="question">Question</label><br>
-            <textarea name="question" value="question" rows="8" cols="40" placeholder="Enter question..." autofocus></textarea><br><br>
-            <label for="answer">Answer</label><br>
-            <textarea name = "answer" value="answer" rows="8" cols="40" placeholder="Enter answer..."></textarea><br><br>
-            <label for="points">Points</label><br>
-            <input type="text" name="points" placeholder="Enter point value..."><br><br>
-            <input type = "submit" name="btn" value="Add" ><br><br>
-        </form>
-        <h2>Questions/Answers Bank</h2>
-        <?php
-        $QuestionBank = util::ForwardGETRequest("question.php", array());
+$QuestionBank = util::ForwardGETRequest("question.php", array());
 
-        if( !$QuestionBank['success']) {
+if(!$QuestionBank['success']) {
+    $view["message"] = $QuestionBank['error'];
+}
 
-            echo 'error';
-            //return;
-        }
-        ?>
-
-        <form name="checkForm" action="question_deletion.php" method="post">
-
-            <style type="text/css">
-                tr, td {border: 1px solid black; }
-                tr.noBorder td {border: 0; }
-
-            </style>
-
-            <table>
-                <tr class ="noBorder">
-                    <td></td>
-                    <td></td>
-                    <td><strong><center>ID</center></strong></td>
-                    <td><strong><center>Question</center></strong></td>
-                    <td><strong><center>Answer</center></strong></td>
-                    <td><strong><center>Points</center></strong</td>
-                </tr>
-                <?php
-
-                $question_html = '
+$question_html = '
 <tr>
     <td><input type="checkbox" name="{{id}}"></td>
     <td><a class="button" href="question_creation.php?edit={{id}}">Edit</a></td>
@@ -91,24 +44,15 @@ hdr("Question Creation");
     <td>{{points}}</td>
 </tr>';
 
-                foreach ( $QuestionBank['result'] as $q ){
-                    $question = array();
-                    $question["id"] = $q['id'];
-                    $question["question"] = util::Printable($q['question']);
-                    $question["answer"] = util::Printable($q['answer']);
-                    $question["points"] = util::Printable($q['points']);
-                    echo render($question_html, $question);
-                }
-                ?>
-            </table>
-            <input type="submit" name="delete" value="Delete"">
-        </form>
+$items = "";
+foreach ( $QuestionBank['result'] as $q ){
+    $question = array();
+    $question["id"] = $q['id'];
+    $question["question"] = util::Printable($q['question']);
+    $question["answer"] = util::Printable($q['answer']);
+    $question["points"] = util::Printable($q['points']);
+    $items .= render($question_html, $question);
+}
 
-        <script>
-            document.getElementById('btnTest').onclick = function(){
-                var selected = getSelectedBox(this.form);
-                alert(selected);      // debug
-            }
-        </script>
-        <div id="message"></div>
-    </center>
+$view["items"] = $items;
+view();
