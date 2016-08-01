05de4ea97d46306b68bceaae102e26ab8701fb13
George Hahn <george@genericmaker.com>
2016-08-01 04:16:46 -0400

    Don't show exam to students if they've already taken it. Closes #28

--- Frontend/exam_list.php
+++ Frontend/exam_list.php
@@ -9,6 +9,19 @@ if($response["success"] == false)
     die("Error: " . $response["error"]);
 $view["exams"] = $response["result"];
 
+$exams = array();
+$results = util::ForwardGetRequest("result.php", array("studentID" => util::GetUserID()));
+foreach ($results["result"] as $r) {
+    if(!in_array($r["exam_id"], $exams))
+        $exams []= $r["exam_id"];
+}
+
+foreach($view["exams"] as $k => $v) {
+    if(in_array($v["id"], $exams)) {
+        unset($view["exams"][$k]);
+    }
+}
+
 foreach($view["exams"] as &$exam) {
     $exam["questioncount"] = count($exam["ownExamquestion"]);
 }
