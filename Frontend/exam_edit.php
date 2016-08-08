b231c644ce7cecea67bac7c17e9ff5d6f7632cc6
garykc <gkc@protonmail.com>
2016-08-07 23:37:07 -0400

    Fix point override, need to fix view

--- Frontend/exam_edit.php
+++ Frontend/exam_edit.php
@@ -27,10 +27,10 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
     $exam["released"] = isset($_POST["released"]);
     $exam["questionIDs"] = array();
 
-    // TODO Edit point overrides
     foreach ($_POST as $key => $value) {
         if(substr($key, 0, 14) === "sharedquestion" && $value === "on") {
             $question_id = (int)substr($key, 14);
+            $question_id = $question_id.":".$_POST["pointoverride".$question_id];
             array_push($exam["questionIDs"], $question_id);
         }
     }
